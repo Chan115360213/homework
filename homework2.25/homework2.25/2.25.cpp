aac9748a6e5b6f83a19696f74c3f8fb63a9a4a4f
Chan115360213 <down961023@gmail.com>
2026-09-22 20:12:56 +0800

    Initial commit

---
+++ homework2.25/homework2.25/2.25.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(void) 
+{
+        printf("CCCCCCCCC\n");
+        printf("C       C\n");
+        printf("C       C\n");
+        printf("C       C\n");
+        printf("CCC    CC\n");
+
+        printf("BBBBBBBBB\n");
+        printf("B   B   B\n");
+        printf("B   B   B\n");
+        printf("B   B   B\n");
+        printf("  B   B \n");
+
+        printf("HHHHHHHHH\n");
+        printf("    H    \n");
+        printf("    H    \n");
+        printf("    H    \n");
+        printf("HHHHHHHHH\n");
+    return 0;
+}
